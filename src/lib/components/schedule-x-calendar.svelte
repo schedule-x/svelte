5dca66e52a74436d1b9c551a6c8c9c0acb83d454
Bryan van der Starre <bryan@starre.dev>
2025-03-22 18:28:52 +0100

    Replaced hardcoded component keys with a dynamic mapped type based on CustomComponentName.

--- src/lib/components/schedule-x-calendar.svelte
+++ src/lib/components/schedule-x-calendar.svelte
@@ -4,78 +4,44 @@
 	import { createCustomComponentFn } from '$lib/utils/create-custom-component-fn.js';
 	import Portal from '$lib/utils/Portal.svelte';
 
-	import type { CustomComponentMeta, CustomComponents } from '$lib/types/custom-components.js';
+	import type { CustomComponentMeta } from '$lib/types/custom-components.js';
 	import type { CalendarApp } from '@schedule-x/calendar';
 
 	export let calendarApp: CalendarApp;
 
-	export let timeGridEvent: CustomComponents['timeGridEvent'] | undefined = undefined;
-	export let dateGridEvent: CustomComponents['dateGridEvent'] | undefined = undefined;
-	export let monthGridEvent: CustomComponents['monthGridEvent'] | undefined = undefined;
-	export let monthAgendaEvent: CustomComponents['monthAgendaEvent'] | undefined = undefined;
-	export let eventModal: CustomComponents['eventModal'] | undefined = undefined;
-	export let headerContentLeftPrepend: CustomComponents['headerContentLeftPrepend'] | undefined =
-		undefined;
-	export let headerContentLeftAppend: CustomComponents['headerContentLeftAppend'] | undefined =
-		undefined;
-	export let headerContentRightPrepend: CustomComponents['headerContentRightPrepend'] | undefined =
-		undefined;
-	export let headerContentRightAppend: CustomComponents['headerContentRightAppend'] | undefined =
-		undefined;
-	export let headerContent: CustomComponents['headerContent'] | undefined = undefined;
-
 	let customComponentsMeta: CustomComponentMeta[] = [];
-
 	$: wrapperId = randomStringId();
-	$: customComponentsMeta = [];
 
 	const setComponent = (component: CustomComponentMeta) => {
 		const newComponents = [...customComponentsMeta];
 		const ccid = component.wrapperElement.dataset.ccid;
-		const existingComponent = newComponents.find((c) => c.wrapperElement.dataset.ccid === ccid);
+		const existing = newComponents.find((c) => c.wrapperElement.dataset.ccid === ccid);
 
-		if (existingComponent) {
-			newComponents.splice(newComponents.indexOf(existingComponent), 1);
+		if (existing) {
+			newComponents.splice(newComponents.indexOf(existing), 1);
 		}
 
 		customComponentsMeta = [...newComponents, component];
 	};
 
 	const setCustomComponentFns = () => {
-		const customComponentsAvailable: {
-			name: keyof CustomComponents;
-			component: CustomComponents[keyof CustomComponents];
-		}[] = [
-			{ name: 'timeGridEvent', component: timeGridEvent },
-			{ name: 'dateGridEvent', component: dateGridEvent },
-			{ name: 'monthGridEvent', component: monthGridEvent },
-			{ name: 'monthAgendaEvent', component: monthAgendaEvent },
-			{ name: 'eventModal', component: eventModal },
-			{ name: 'headerContentLeftPrepend', component: headerContentLeftPrepend },
-			{ name: 'headerContentLeftAppend', component: headerContentLeftAppend },
-			{ name: 'headerContentRightPrepend', component: headerContentRightPrepend },
-			{ name: 'headerContentRightAppend', component: headerContentRightAppend },
-			{ name: 'headerContent', component: headerContent }
-		];
-
-		customComponentsAvailable.forEach(({ name, component }) => {
+		Object.entries($$restProps).forEach(([name, component]) => {
 			if (component) {
-				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-				// @ts-ignore
 				calendarApp._setCustomComponentFn(name, createCustomComponentFn(setComponent, component));
 			}
 		});
 	};
 
-	onMount(() => {
+	$: if (calendarApp) {
 		setCustomComponentFns();
+	}
 
+	onMount(() => {
 		const wrapper = document.getElementById(wrapperId);
 		if (!(wrapper instanceof HTMLElement)) {
 			console.warn('Could not find wrapper element to mount calendar on');
 			return;
 		}
-
 		calendarApp.render(wrapper);
 	});
 </script>
---
+++ src/routes/components/week-grid-date.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let date: Date;
+
+	const formatter = new Intl.DateTimeFormat('en-US', { weekday: 'short' });
+	const day = formatter.format(new Date(date)).toUpperCase();
+</script>
+
+<span class="custom-week-grid-date">
+	{day}
+</span>
