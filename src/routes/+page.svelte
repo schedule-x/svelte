589de8322dfeb70b1e9a6c646db95109076aa991
Tom Österlund <tomosterlund@macbookpro.speedport.ip>
2025-08-20 20:27:33 +0200

    feat: v3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
 	import TimeGridEvent from './components/time-grid-event.svelte';
 	import HeaderContent from './components/header-content.svelte';
 	import WeekGridDate from './components/week-grid-date.svelte';
+	import 'temporal-polyfill/global';
+	import { Temporal } from 'temporal-polyfill';
 
 	import { createCalendar, viewDay, viewWeek } from '@schedule-x/calendar';
 
@@ -11,30 +13,31 @@
 		plugins: [],
 		views: [viewDay, viewWeek],
 		defaultView: viewWeek.name,
+		selectedDate: Temporal.PlainDate.from('2024-07-05'),
 		events: [
 			{
 				id: 1,
 				title: 'Event 1',
-				start: '2024-07-05 01:00',
-				end: '2024-07-05 02:00'
+				start: Temporal.ZonedDateTime.from('2024-07-05T01:00:00+09:00[Asia/Tokyo]'),
+				end: Temporal.ZonedDateTime.from('2024-07-05T02:00:00+09:00[Asia/Tokyo]')
 			},
 			{
 				id: 2,
 				title: 'Event 2',
-				start: '2024-07-05 02:00',
-				end: '2024-07-05 03:00'
+				start: Temporal.ZonedDateTime.from('2024-07-05T02:00:00+09:00[Asia/Tokyo]'),
+				end: Temporal.ZonedDateTime.from('2024-07-05T03:00:00+09:00[Asia/Tokyo]')
 			},
 			{
 				id: 3,
 				title: 'Event 3',
-				start: '2024-07-05 02:00',
-				end: '2024-07-05 03:00'
+				start: Temporal.ZonedDateTime.from('2024-07-05T02:00:00+09:00[Asia/Tokyo]'),
+				end: Temporal.ZonedDateTime.from('2024-07-05T03:00:00+09:00[Asia/Tokyo]')
 			},
 			{
 				id: 4,
 				title: 'Event 4',
-				start: '2024-07-05',
-				end: '2024-07-05'
+				start: Temporal.PlainDate.from('2024-07-05'),
+				end: Temporal.PlainDate.from('2024-07-05')
 			}
 		]
 	});
